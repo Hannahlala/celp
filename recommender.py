710e77ab9442b95f16592260464db2c5b0d5c389
Hannahlala <46671726+Hannahlala@users.noreply.github.com>
2019-05-11 14:49:28 +0200

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,6 +1,9 @@
 from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
 
 import random
+import numpy as np
+from operator import itemgetter
+
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
@@ -17,27 +20,76 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
     """
     # ideeën
     # op plek x een random suggestie (weinig rating of buiten range)
-    
-    print(user_id, business_id, city, n)
-    if not city:
-        city = random.choice(CITIES)
-    
+
     if not user_id:
-        # filter all data (nu elke keer als pagina wordt geopend, moet per sessie)
-        filtered_data =[]
+        if not business_id:
+            return logout_without_business(city, n)
+        else:
+            return logout_with_business(business_id, city, n)
+    # city = random.choice(CITIES)
+
+    if not business_id:
+        return login_without_business(user_id, city, n)
+
+    return login_with_business(user_id, business_id, city, n)
+
+
+def logout_without_business(city, n):
+    # filter all data (nu elke keer als pagina wordt geopend, moet ooit per sessie)
+    filtered_data = []
+    if not city:
         for place in CITIES:
             for business in BUSINESSES[place]:
                 if business['is_open'] == 1 and business['review_count'] > 9:
                     filtered_data.append(business)
-        
-        
-        sorted_data = sorted(filtered_data, key=lambda k: k['stars'], reverse=True)[:n+1]
-        
-        # Check if searched business is not also recommended, remove if yes
-        for x in sorted_data:
-            if x['business_id'] == business_id:
-                sorted_data.remove(x)
-    
-        # let op, als er geen overlap was, geeft hij nu 11 terug ipv 10
-        return sorted_data[:n]
+
+    else:
+        for business in BUSINESSES[city]:
+            if business['is_open'] == 1 and business['review_count'] > 9:
+                filtered_data.append(business)
+
+    sorted_data = sorted(filtered_data, key=itemgetter('stars'), reverse=True)[:n + 1]
+
+    # let op, als er geen overlap was, geeft hij nu 11 terug ipv 10
+    return sorted_data[:n]
+
+
+def logout_with_business(business_id, city, n):
+    if not city:
+        city = random.choice(CITIES)
+
+    # get categories from specific business
+    for business1 in BUSINESSES[city]:
+        if business1["business_id"] == business_id:
+            business_cat = business1["categories"].split(', ')
+
+    # check if categories match with other businesses
+    filtered_data = []
+    for business2 in BUSINESSES[city]:
+        if any(x in business2["categories"].split(', ') for x in business_cat):
+            filtered_data.append(business2)
+
+    sorted_data = sorted(filtered_data, key=itemgetter('stars'), reverse=True)[:n + 1]
+
+    # prevents the specific business from displaying twice
+    for x in sorted_data:
+        if x['business_id'] == business_id:
+            sorted_data.remove(x)
+
+    return  sorted_data[:n]
+
+
+def login_without_business(user_id, city, n):
+    if not city:
+        city = random.choice(CITIES)
+
+    for review in REVIEWS[city]:
+        if review["user_id"] == user_id:
+            print("kom ik hier?")
+    return random.sample(BUSINESSES[city], n)
+
+
+def login_with_business(user_id, business_id, city, n):
+    if not city:
+        city = random.choice(CITIES)
     return random.sample(BUSINESSES[city], n)
