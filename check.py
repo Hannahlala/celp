be13d19eb4750c2c24f244e53c65bffc9b08faf1
Sarah <sarah.tol@live.nl>
2019-05-14 10:36:03 +0200

    Create check.py
    Split functie werkt

---
+++ check.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
+
+
+frame = pd.concat([pd.DataFrame(BUSINESSES[x]) for x in BUSINESSES])
+
+trainset, testset = train_test_split(frame, test_size=0.20)
